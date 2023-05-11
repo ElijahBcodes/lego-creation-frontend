218e28d95430d31a589149b4388775526b1f0c07
Elijah Barreto <51147388+ElijahBcodes@users.noreply.github.com>
2023-05-10 20:55:47 -0700

    Creation Index, and route

---
+++ src/pages/CreationIndex.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const CreationIndex = ({ creations }) => {
+  console.log(creations)
+  return (
+    <>
+      <h3>Lego Creation index Page</h3>
+    </>
+  )
+}
+
+export default CreationIndex
