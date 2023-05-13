af215e78a8b5552c7d5f8e58aaab696042e15649
Elijah Barreto <51147388+ElijahBcodes@users.noreply.github.com>
2023-05-12 17:51:35 -0700

    Initial setup, frontend structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import { Routes, Route } from "react-router-dom"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import CreationEdit from "./pages/CreationEdit"
+import CreationIndex from "./pages/CreationIndex"
+import CreationNew from "./pages/CreationNew"
+import CreationShow from "./pages/CreationShow"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import "./App.css"
+
+
+
+
+
+
+
 
 function App() {
   return (
     <>
+    < Header/> 
+    
     <Routes>
       <Route path="/creationindex" element={<CreationIndex />} />
     </Routes>
---
+++ src/pages/CreationShow.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreationShow = () => {
+  return (
+    <div>CreationShow</div>
+  )
+}
+
+export default CreationShow
