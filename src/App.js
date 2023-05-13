b641ed6a3fb8016a5f0bc5d7e60fadd4c80e7a12
Elijah Barreto <51147388+ElijahBcodes@users.noreply.github.com>
2023-05-13 07:23:47 -0700

    File structure

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import { Routes, Route } from "react-router-dom"
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-import CreationEdit from "./pages/CreationEdit"
-import CreationIndex from "./pages/CreationIndex"
-import CreationNew from "./pages/CreationNew"
-import CreationShow from "./pages/CreationShow"
-import Home from "./pages/Home"
-import NotFound from "./pages/NotFound"
-import "./App.css"
+import { Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import CreationEdit from "./pages/CreationEdit";
+import CreationIndex from "./pages/CreationIndex";
+import CreationNew from "./pages/CreationNew";
+import CreationShow from "./pages/CreationShow";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+// import "./App.css"
 
+import mockCreations from "./mockCreations";
 
+const App = () => {
+  const [creations, setCreations] = useState(mockCreations);
 
+  console.log(creations);
 
-
-
-
-
-function App() {
   return (
     <>
-    < Header/> 
-    
-    <Routes>
-      <Route path="/creationindex" element={<CreationIndex />} />
-    </Routes>
+      <Header />
+      <Home />
+      <CreationIndex />
+      <CreationShow />
+      <CreationNew />
+      <CreationEdit />
+      <NotFound />
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
