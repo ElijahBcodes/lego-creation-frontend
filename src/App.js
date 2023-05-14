8135200b89407b112b391baac35b6e19643519c3
Elijah Barreto <51147388+ElijahBcodes@users.noreply.github.com>
2023-05-13 21:04:57 -0700

    Routes finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import React, { useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -8,27 +8,37 @@ import CreationNew from "./pages/CreationNew";
 import CreationShow from "./pages/CreationShow";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-// import "./App.css"
 
+//mockData
 import mockCreations from "./mockCreations";
 
-const App = () => {
+function App () {
   const [creations, setCreations] = useState(mockCreations);
-
   console.log(creations);
+
+  const createCreation =(creation) => {
+    console.log(creation);
+  }
+
+  const updateCreation =(creation, id) => {
+    console.log("creation:", creation);
+    console.log("id:", id);
+  }
 
   return (
     <>
-      <Header />
-      <Home />
-      <CreationIndex />
-      <CreationShow />
-      <CreationNew />
-      <CreationEdit />
-      <NotFound />
-      <Footer />
-    </>
-  );
+    <Header />
+      <Routes>
+        <Route path="/" element={ <Home/>}/>
+        <Route path="/creationindex" element={<CreationIndex creations={creations}/> }/>
+        <Route path="/creationshow" element={<CreationShow creations={creations}/>} />
+        <Route path="/creationnew" element={<CreationNew creations={creations}/>} />
+        <Route path="/creationedit/:id" element={<CreationEdit creations={creations} updateCreation={updateCreation}/>} />
+        <Route path="*" element={<NotFound />} />
+     </Routes>
+     <Footer />
+  </>
+  )
 };
 
 export default App;
