b482900065c2111121fdd2c2534e8df5199e02c4
ElijahBcodes <51147388+ElijahBcodes@users.noreply.github.com>
2024-02-04 16:37:24 -0800

    show page, fix home

--- src/pages/CreationShow.js
+++ src/pages/CreationShow.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
-const CreationShow = () => {
+
+const CreationShow = ({creations, updateCreation}) => {
+const {id} = useParams()
+console.log(id)
+const currentCreation = creation
+
+
+  
   return (
     <div>CreationShow</div>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,36 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="hero min-h-screen bg-base-200" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1633469924738-52101af51d87?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bGVnb3xlbnwwfHwwfHx8MA%3D%3D)'}} >
+    <div className="hero-content flex-col lg:flex-row-reverse">
+      <div className="text-center lg:text-left">
+        <h1 className="text-5xl font-bold text-white">Welcome to the Lego Creations Site </h1>
+        <p className="py-6 text-white">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      </div>
+      <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <form className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input type="email" placeholder="email" className="input input-bordered" required />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input type="password" placeholder="password" className="input input-bordered" required />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
   )
 }
 
