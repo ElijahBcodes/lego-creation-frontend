43c61334b1d87c90d9f710349603a24670055a61
ElijahBcodes <51147388+ElijahBcodes@users.noreply.github.com>
2024-02-08 11:41:04 -0800

    add flex to index

--- src/pages/CreationIndex.js
+++ src/pages/CreationIndex.js
@@ -4,15 +4,15 @@ import "../App.css";
 
 const CreationIndex = ({ creations }) => {
   return (
-    <main>
+    <main className="flex flex-wrap justify-around">
       {creations.map((creation, index) => {
         return (
           <>
-            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card w-96 bg-base-100 shadow-xl flex">
               <figure>
                 <img src={creation.image} alt="picture of creation" />
               </figure>
-              <div className="card-body">
+              <div className="card-body flex-wrap">
                 <h2 className="card-title">{creation.name}</h2>
                 <p>{creation.description}</p>
                 <div className="card-actions justify-end">
