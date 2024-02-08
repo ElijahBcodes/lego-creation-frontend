db91712067da7f1c54fbcd73b7bc4a5ec7280a42
ElijahBcodes <51147388+ElijahBcodes@users.noreply.github.com>
2024-02-08 11:29:01 -0800

    patch

--- src/mockCreations.js
+++ src/mockCreations.js
@@ -8,7 +8,7 @@ const creations = [
     {
         id:2,
         name:"Sailer Man", 
-        description:"An old sailor with a giant grey beard and bald head. Life size wearing a red and yellow stripe sweater, wPlaying a flute",
+        description:"An old sailor with a giant grey beard and bald head. Life size wearing a red and yellow stripe sweater, playing a flute.",
         image:"https://c1.wallpaperflare.com/preview/469/621/110/legoland-building-blocks-legos-lego.jpg"
     },
     {
@@ -20,13 +20,13 @@ const creations = [
     {
         id:4,
         name:"Friends", 
-        description:"Friends having coffee sitting together",
+        description:"Friends having coffee sitting together.",
         image:"https://images.unsplash.com/photo-1607297737950-b3c024a71a69?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzV8fGxlZ298ZW58MHx8MHx8fDA%3D"
     },
     {
         id:5,
         name:"Miami Beach", 
-        description:"Beach house with a very colorful exterior",
+        description:"Beach house with a very colorful exterior.",
         image:"https://images.unsplash.com/photo-1605012464390-45820d1f7bdf?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzZ8fGxlZ28lMjBmb3J0bml0ZXxlbnwwfHwwfHx8MA%3D%3D"
     }
 ]
