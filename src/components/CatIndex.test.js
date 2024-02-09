3ec8fa030f2dd267dc202c2088a35673e53fa0d8
ElijahBcodes <51147388+ElijahBcodes@users.noreply.github.com>
2024-02-09 14:38:32 -0800

    testing for index page

---
+++ src/components/CatIndex.test.js
@@ -0,0 +1,7 @@
+import { render } from "@testing-library/react"
+import CreationIndex from "../pages/CreationIndex"
+
+describe("<CatIndex />", () => {
+  it("renders without crashing", () => {})
+  it("renders cat cards", () => {})
+})
