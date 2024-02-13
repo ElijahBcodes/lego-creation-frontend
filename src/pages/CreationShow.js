da6495fa1b37d0d786114101a62ceff89b11e0bf
ElijahBcodes <51147388+ElijahBcodes@users.noreply.github.com>
2024-02-12 17:10:34 -0800

    show page passing data, cards display

--- src/pages/CreationShow.js
+++ src/pages/CreationShow.js
@@ -1,18 +1,26 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
+import "../App.css";
 
+const CreationShow = ({ creations }) => {
+  const { id } = useParams();
+  let currentCreation = creations.find((creation) => creation.id === +id);
 
-const CreationShow = ( {creations}) => {
-  console.Console.log(creations)
-
-
-const CreationShow = ({creations, updateCreation}) => {
- 
-  
+  console.log(currentCreation);
 
   return (
-    <div>CreationShow</div>
-  )
-}
+    <div className="card w-96 bg-base-100 shadow-xl flex" key={id}>
+      <figure>
+        <img src={currentCreation.image} alt="picture of creation" />
+      </figure>
+      <div className="card-body flex-wrap">
+        <h2 className="card-title">{currentCreation.name}</h2>
+        <p>{currentCreation.description}</p>
 
-export default CreationShow
+        <div className="card-actions justify-end"></div>
+      </div>
+    </div>
+  );
+};
+
+export default CreationShow;
