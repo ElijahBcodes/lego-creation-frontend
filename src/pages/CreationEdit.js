c028890798ebb7702b91124c11bb2134b3ad7de8
ElijahBcodes <51147388+ElijahBcodes@users.noreply.github.com>
2024-02-22 21:26:13 -0800

    edit page handleSubmit and change update. Data is passing

--- src/pages/CreationEdit.js
+++ src/pages/CreationEdit.js
@@ -1,52 +1,47 @@
-import React, {  useState, } from 'react'
-import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
-import { useNavigate, useParams  } from 'react-router-dom'
+import React, { useState } from "react";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { useNavigate, useParams } from "react-router-dom";
 
 const CreationEdit = ({ creations, updateCreation }) => {
-  const { id } = useParams()
-  let currentCreation = creations?.find((creation) => creation.id === +id)
+  const { id } = useParams();
+  let currentCreation = creations?.find((creation) => creation.id === +id);
 
   const [editCreation, setEditCreation] = useState({
     name: currentCreation.name,
     description: currentCreation.description,
-    image: currentCreation.image
-  })
-  const handleSubmit = (e) => {
-    setEditCreation({ ...editCreation, [e.target.name]: e.target.value})
-  }
-  const navigate = useNavigate()
+    image: currentCreation.image,
+  });
+  const handleChange = (e) => {
+    setEditCreation({ ...editCreation, [e.target.name]: e.target.value });
+  };
+  const navigate = useNavigate();
   const handleSubmit = () => {
-    updateCat(editCreation, currentCreation.id)
-    navigate("/creationindex")
-  }
+    updateCreation(editCreation, currentCreation.id);
+    navigate("/creationindex");
+  };
 
-
-
-
-
-       return (
+  return (
     <Form>
-    <FormGroup>
-      <Label for="name">Name</Label>
-      <Input type="text" name="name" />
-    </FormGroup>
-    <FormGroup>
-      <Label for="description">Description</Label>
-      <Input type="text" name="description" />
-    </FormGroup>
-    <FormGroup>
-      <Label for="enjoys">Enjoys</Label>
-      <Input type="text" name="enjoys" />
-    </FormGroup>
-    <FormGroup>
-      <Label for="image">Image URL</Label>
-      <Input type="text" name="image" />
-    </FormGroup>
-    <Button onClick={handleSubmit} name="submit">
-          Save
-        </Button>
-  </Form>
-  )
-}
+      <FormGroup>
+        <Label for="name">Name</Label>
+        <Input type="text" name="name" onChange={handleChange}
+            value={editCreation.name} />
+      </FormGroup>
+      <FormGroup>
+        <Label for="description">Description</Label>
+        <Input type="text" name="description" onChange={handleChange}
+            value={editCreation.description} />
+      </FormGroup>
+      <FormGroup>
+        <Label for="image">Image URL</Label>
+        <Input type="text" name="image" onChange={handleChange}
+            value={editCreation.image}/>
+      </FormGroup>
+      <Button onClick={handleSubmit} name="submit">
+        Save
+      </Button>
+    </Form>
+  );
+};
 
-export default CreationEdit
+export default CreationEdit;
