3988830fecacff544ea50485c06d89774300f733
ElijahBcodes <51147388+ElijahBcodes@users.noreply.github.com>
2024-03-05 20:46:31 -0800

    delete handle works

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import CreationNew from "./pages/CreationNew";
 import CreationShow from "./pages/CreationShow";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
-// import mockCreations from './mockCreations.js';
+
 
 
 function App () {
@@ -27,14 +27,41 @@ function App () {
       }).catch((error) => console.log(error))
   }
 
-
-
   const createCreation = (creation) => {
-  };
+    fetch("http://localhost:3000/creations ",{
+    body: JSON.stringify(creation),
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: "POST"
+  })
+    .then((response) => response.json())
+    .then((payload) => this.readCreation())
+    .catch((errors) => console.log("Creation create errors:", errors))
+}
 
-  const updateCreation =(creation, id) => {
-    console.log("creation:", creation)
-    console.log ("id:", id)
+  const updateCreation = (creation, id) => {
+    fetch(`http://localhost:3000/creations/${id}`, {
+      body: JSON.stringify(creation),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "PATCH"
+    })
+      .then((response) => response.json())
+      .then((payload) => this.readCreation())
+      .catch((errors) => console.log("Creation update errors:", errors))
+  }
+  const deleteCreation = (id) => {
+    fetch(`http://localhost:3000/creations/${id}`, {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "DELETE"
+    })
+      .then((response) => response.json())
+      .then((payload) => this.readCreation())
+      .catch((errors) => console.log("delete errors:", errors))
   }
 
   return (
@@ -43,9 +70,10 @@ function App () {
       <Routes>
         <Route path="/" element={ <Home/>}/>
         <Route path="/creationindex" element={<CreationIndex creations={creations} /> }/>
-        <Route path="/creationshow/:id" element={<CreationShow creations={creations}/>} />
+        <Route path="/creationshow/:id" element={<CreationShow creations={creations} deleteCreation={deleteCreation}/>} />
         <Route path="/creationnew" element={<CreationNew createCreation={createCreation}/>} />
         <Route path="/creationedit/:id" element={<CreationEdit creations={creations} updateCreation={updateCreation}/>} />
+        
         <Route path="*" element={<NotFound/>} /> 
      </Routes>
   </>
--- src/pages/CreationEdit.js
+++ src/pages/CreationEdit.js
@@ -19,26 +19,57 @@ const CreationEdit = ({ creations, updateCreation }) => {
     updateCreation(editCreation, currentCreation.id);
     navigate("/creationindex");
   };
+  const handleDelete = () => {
+    deleteCreation(id);
+    navigate("/creationsindex");
+    console.log(currentCreation);
+  };
+  const deleteCreation = (id) => {
+    fetch(`http://localhost:3000/creations/${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((payload) => this.readCreation())
+      .catch((errors) => console.log("delete errors:", errors));
+  };
 
   return (
     <Form>
       <FormGroup>
         <Label for="name">Name</Label>
-        <Input type="text" name="name" onChange={handleChange}
-            value={editCreation.name} />
+        <Input
+          type="text"
+          name="name"
+          onChange={handleChange}
+          value={editCreation.name}
+        />
       </FormGroup>
       <FormGroup>
         <Label for="description">Description</Label>
-        <Input type="text" name="description" onChange={handleChange}
-            value={editCreation.description} />
+        <Input
+          type="text"
+          name="description"
+          onChange={handleChange}
+          value={editCreation.description}
+        />
       </FormGroup>
       <FormGroup>
         <Label for="image">Image URL</Label>
-        <Input type="text" name="image" onChange={handleChange}
-            value={editCreation.image}/>
+        <Input
+          type="text"
+          name="image"
+          onChange={handleChange}
+          value={editCreation.image}
+        />
       </FormGroup>
       <Button onClick={handleSubmit} name="submit">
         Save
+      </Button>
+      <Button onClick={handleDelete} color="danger">
+        Delete
       </Button>
     </Form>
   );
